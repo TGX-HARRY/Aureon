bdc08051e755cc954a77cffb103d12b12f64d12b
Hemang Mittal <mittalhemang11@gmail.com>
2026-03-02 17:44:50 +0530

    Reverted changes

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// app.js
-
 const express = require("express");
 const userRoutes = require("./Routing/user.routes");
-const { applyMiddlewares, setupRoutes } = require("./utils/helper");
 
 const app = express();
 
-// apply middleware
-applyMiddlewares(app);
+app.use(express.json());
+app.use(express.static("public"));
 
-// setup routes
-setupRoutes(app, userRoutes);
+app.use("/api", userRoutes);
 
 module.exports = app;
