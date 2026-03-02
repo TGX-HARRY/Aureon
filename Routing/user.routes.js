eaf35b701ab6c689380c55d9e69117d3a9079bfb
Hemang Mittal <mittalhemang11@gmail.com>
2026-03-03 03:55:15 +0530

    Added routes for getting user movies list and fixed routing for user login

--- Routing/user.routes.js
+++ Routing/user.routes.js
@@ -5,11 +5,12 @@ const router = express.Router();
 
 // router.get("/subscribers", controllers.getSubscribers);
 router.get("/users/admins", controllers.getAdmins);
-router.post("/users/subscribers/login", controllers.fetchSubscriber);
+router.get("/movies/mylist/:id", controllers.fetchUserMovieList);
 router.get("/movies", controllers.fetchMoviesData);
 router.get("/subscribers/:id", controllers.getUsersDataByID);
 router.post("/users/subscribers/register", controllers.addSubscriber);
 router.post("/users/admins/register", controllers.addAdmin);
+router.post("/users/subscribers/login", controllers.fetchSubscriber);
 // router.patch("/admins/changeinfo/:email", controllers.changeAdminInfo);
 // router.patch("/customers/changeinfo/:email", controllers.changeSubscriberInfo);
 // router.patch("/admins/changepassword/:email", controllers.changeAdminPassword);
