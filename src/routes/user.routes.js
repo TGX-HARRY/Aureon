0c094b52247740a32e44f3db3b9c8ab2d84ec3ba
Hemang Mittal <mittalhemang11@gmail.com>
2026-03-05 20:00:53 +0530

    Added routes to get Subcribers and Admins data, change password (subscriber only) and minor fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,18 +3,18 @@ const controllers = require("../controllers/user.controller");
 
 const router = express.Router();
 
-// router.get("/subscribers", controllers.getSubscribers);
-router.get("/users/admins", controllers.getAdmins);
+router.get("/users/subscribers/:id", controllers.getSubscribers);
+router.get("/users/admins/:id", controllers.getAdmins);
 router.get("/movies/mylist/:id", controllers.fetchUserMovieList);
 router.get("/movies", controllers.fetchMoviesData);
-router.get("/subscribers/:id", controllers.getUsersDataByID);
+router.get("/users/subscribers/:id", controllers.getUsersDataByID);
 router.post("/users/subscribers/register", controllers.addSubscriber);
 router.post("/users/admins/register", controllers.addAdmin);
 router.post("/users/subscribers/login", controllers.fetchSubscriber);
 // router.patch("/admins/changeinfo/:email", controllers.changeAdminInfo);
 router.patch("/users/subscribers/changeinfo", controllers.changeSubscriberInfo);
 // router.patch("/admins/changepassword/:email", controllers.changeAdminPassword);
-// router.patch("/customers/changepassword/:email", controllers.changeSubscriberPassword);
+router.patch("/users/subscribers/changepassword", controllers.changeSubscriberPassword);
 router.delete("/users/subscribers/remove/:id", controllers.removeSubscriberAccount);
 // router.delete("/admins/remove/:email", controllers.removeAdminAccount);
 
