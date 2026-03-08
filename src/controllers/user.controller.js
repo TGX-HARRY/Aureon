07f7850809fca54fd366c4cbbb7e621f62c11fe6
mahesh4596 <maheshsingla2007@gmail.com>
2026-03-08 18:01:22 +0530

    small changes in controllers and routes folder

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,15 @@
-const {getUsersData, writeUserData, rewriteUserData, getMoviesData, getUserMovies} = require("../utils/file.utils");
+const { getUsersData, writeUserData, rewriteUserData, getMoviesData, getUserMovies } = require("../utils/file.utils");
 const bcrypt = require("bcrypt");
 
 let sessionID;
-exports.getSessionID = () => {
+const getSessionID = () => {
     return sessionID;
 }
 
-function generateSessionID (){
+function generateSessionID() {
     sessionID = crypto.randomUUID();
     return sessionID;
-} 
+}
 
 async function checkUserData(email) {
     const existingData = await getUsersData();
@@ -23,7 +23,7 @@ async function userLookupWithID(id) {
     return userGroup.find(u => u.id === id) || null;
 }
 
-exports.addSubscriber = async (req, res) => {
+const addSubscriber = async (req, res) => {
 
     const present = await checkUserData(req.body.email);
 
@@ -52,7 +52,7 @@ exports.addSubscriber = async (req, res) => {
     });
 };
 
-exports.fetchSubscriber = async (req, res) => {
+const fetchSubscriber = async (req, res) => {
     const isEmailPresent = await checkUserData(req.body.email);
 
     if (isEmailPresent) {
@@ -60,11 +60,11 @@ exports.fetchSubscriber = async (req, res) => {
         const isMatch = await bcrypt.compare(req.body.password, isEmailPresent.password);
         if (isMatch) {
             // Hash matched, user logged in succesfully
-            const sessionData =  {
-                "name" : isEmailPresent.name, 
-                "id" : isEmailPresent.id, 
-                "userType" : "subscriber",
-                "sessionID" : generateSessionID()
+            const sessionData = {
+                "name": isEmailPresent.name,
+                "id": isEmailPresent.id,
+                "userType": "subscriber",
+                "sessionID": generateSessionID()
             };
             return res.status(200).json({ sessionData });
         }
@@ -77,15 +77,15 @@ exports.fetchSubscriber = async (req, res) => {
     }
 };
 
-exports.getSubscribers = async (req, res) => {
+const getSubscribers = async (req, res) => {
     const adminid = req.params.id;
     if (!adminid) {
-        return res.status(400).json({message : "ID not provided!"});
+        return res.status(400).json({ message: "ID not provided!" });
     }
 
     const validID = await userLookupWithID(adminid, "admin");
     if (!validID) {
-        return res.status(401).json({message : "Permission Denied"});
+        return res.status(401).json({ message: "Permission Denied" });
     }
 
     const existingData = getUsersData();
@@ -98,10 +98,10 @@ exports.getSubscribers = async (req, res) => {
     if (customersDataWithoutPassword.length === 0) {
         return res.status(404).json({ message: "No subscribers found" });
     }
-    return res.status(200).json({data : customersDataWithoutPassword});
+    return res.status(200).json({ data: customersDataWithoutPassword });
 }
 
-exports.changeSubscriberInfo = async (req, res) => {
+const changeSubscriberInfo = async (req, res) => {
     const email = req.body.email;
     const existingData = await getUsersData();
     const user = existingData.subscribers.find(u => u.email === email);
@@ -116,15 +116,15 @@ exports.changeSubscriberInfo = async (req, res) => {
     }
 
     if (user) {
-    user.name = req.body.name || user.name;
-    user.phone = req.body.phone || user.phone;
-    user.fullName = req.body.fullName || user.fullName;
-    user.gender = req.body.gender || user.gender;
-    user.address = req.body.address || user.address;
-    user.dob = req.body.dob || user.dob;
-    user.avatar = req.body.avatar || user.avatar;
-}
-    
+        user.name = req.body.name || user.name;
+        user.phone = req.body.phone || user.phone;
+        user.fullName = req.body.fullName || user.fullName;
+        user.gender = req.body.gender || user.gender;
+        user.address = req.body.address || user.address;
+        user.dob = req.body.dob || user.dob;
+        user.avatar = req.body.avatar || user.avatar;
+    }
+
     try {
         await rewriteUserData(existingData);
     } catch (err) {
@@ -134,18 +134,19 @@ exports.changeSubscriberInfo = async (req, res) => {
     return res.status(200).json({ message: "User data updated successfully" });
 }
 
-exports.removeSubscriberAccount = async (req, res) => {
+const removeSubscriberAccount = async (req, res) => {
     const id = req.params.id;
     if (!id) {
-        return res.status(400).json({message : "Invalid ID!"})
+        return res.status(400).json({ message: "Invalid ID!" })
     }
+
     const isPresent = await userLookupWithID(id);
     if (!isPresent) {
-        return res.status(400).json({message : "User does not exist!"});
+        return res.status(400).json({ message: "User does not exist!" });
     }
 
     const data = await getUsersData();
-    
+
     const filteredData = data.subscribers.filter(u => u.id !== id);
 
     try {
@@ -155,56 +156,58 @@ exports.removeSubscriberAccount = async (req, res) => {
         });
     }
     catch (err) {
-        return res.status(400).json({ message : "Data write failed!"});
+        return res.status(400).json({ message: "Data write failed!" });
     }
-    return res.status(200).json({ message : "Subscriber removed successfully!"});
+    return res.json("Subscriber removed successfully!");
 }
 
-exports.fetchMoviesData = async (req, res) => {
+const fetchMoviesData = async (req, res) => {
     const data = await getMoviesData();
-    return (!data)?res.status(200).json({message : "Movies not found"}) : res.status(200).json({data});
+    return (!data) ? res.status(400).json({ message: "Movies not found" }) : res.json(data);
 }
 
-exports.getUsersDataByID = async (req, res) => {
+const getUsersDataByID = async (req, res) => {
     const id = req.params.id;
     if (!id) {
-        return res.status(400).json({message : "Invalid ID!"})
+        return res.status(400).json({ message: "Invalid ID!" })
     }
     const data = await userLookupWithID(id, "subscriber");
     if (!data) {
-        return res.status(400).json({message : "Data couldn't be fetched!"});
+        return res.status(400).json({ message: "Data couldn't be fetched!" });
     }
-    return res.status(200).json({fetchedData: data});
+    return res.json(data);
 }
 
-exports.fetchUserMovieList = async (req, res) => {
-    const response = await getUserMovies(req.params.id);
-    return res.status(200).json({movieList : response}); 
+const fetchUserMovieList = async (req, res) => {
+    const movies = await getUserMovies(req.params.id);
+    return res.json(movies);
 }
 
-exports.changeSubscriberPassword = async (req, res) => {
+const changeSubscriberPassword = async (req, res) => {
     const email = req.body.email;
     if (!email) {
-        return res.status(400).json({message : "User email not found!"});
+        return res.status(400).json({ message: "User email not found!" });
     }
 
     const existingUserData = await getUsersData();
     const user = existingUserData.subscribers.find(u => u.email === email);
     if (!user) {
-        return res.status(400).json({message : "User not found!"})
+        return res.status(400).json({ message: "User not found!" })
     }
 
     if (user) {
         user.password = await bcrypt.hash(req.body.password, 10);
     }
-        
+
     try {
         await rewriteUserData(existingUserData);
     }
     catch (err) {
-        res.status(400).json({messae: "Could rewrite data, due to error : " + err});
+        return res.status(400).json({ message: "Could not rewrite data, due to error : " + err });
     }
 
     // if no error
-    return res.status(200).json({message :  "User password changed successfully!"});
+    return res.status(200).json({ message: "User password changed successfully!" });
 }
+
+module.exports = {addSubscriber, fetchSubscriber, getSubscribers, changeSubscriberInfo, removeSubscriberAccount, fetchMoviesData, getUsersDataByID, fetchUserMovieList, changeSubscriberPassword, getSessionID};
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,12 +3,12 @@ const controllers = require("../controllers/admin.controller");
 
 const router = express.Router();
 
-router.get("/admins/getadmins/:id", controllers.getAdmins);
+router.get("/getadmins/:id", controllers.getAdmins);
 router.get("/movies/count", controllers.getMovieCount);
-router.get("/admins/verify/:id", controllers.checkAdminID);
-router.post("/admins/register", controllers.addAdmin);
-// router.delete("/admins/remove/:email", controllers.removeAdminAccount);
-// router.patch("/admins/changepassword/:email", controllers.changeAdminPassword);
-// router.patch("/admins/changeinfo/:email", controllers.changeAdminInfo);
+router.get("/verify/:id", controllers.checkAdminID);
+router.post("/register", controllers.addAdmin);
+// router.delete("/remove/:email", controllers.removeAdminAccount);
+// router.patch("/changepassword/:email", controllers.changeAdminPassword);
+// router.patch("/changeinfo/:email", controllers.changeAdminInfo);
 
 module.exports = router;
