35eac5d4c585ad8e5e1983183787a0c5f69f9697
Prisha Aggarwal <prisha230906@gmail.com>
2026-03-08 13:52:02 +0530

    Enabled app to use separate routing for admin functioning

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const userRoutes = require("./routes/user.routes");
-
+const adminRoutes = require("./routes/admin.routes");
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 module.exports = app;
