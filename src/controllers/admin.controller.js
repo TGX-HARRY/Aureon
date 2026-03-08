94c2966494226a38aabba1b01c42079c3a7346fe
Prisha Aggarwal <prisha230906@gmail.com>
2026-03-08 13:53:21 +0530

    Added admin methods to separate controller file for admin

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,76 @@
+const {getUsersData, writeUserData, rewriteUserData, getMovieCount} = require("../utils/file.utils");
+
+exports.addAdmin = async (req, res) => {
+    const id = req.body.id;
+    if (!id) {
+        return res.status(400).json({message : "No ID provided!"});
+    }
+    const validID = await userLookupWithID(id, "admin");
+    if (!validID) {
+        return res.status(401).json({message : "Access Denied!"});
+    }
+
+    const present = await checkUserData(req.body.email, "admin");
+
+    if (present) {
+        return res.status(400).json({
+            message: "Account already exists!"
+        });
+    }
+
+    const newAdmin = {
+        id: crypto.randomUUID(),
+        ...req.body,
+        role : "admin"
+    };
+
+    await writeUserData(newAdmin);
+
+    return res.status(200).json({
+        message : "Admin created successfully!"
+    })
+};
+
+exports.getAdmins = async (req, res) => {
+    const adminid = req.body.id;
+    if (!adminid) {
+        return res.status(400).json({message : "ID not provided!"});
+    }
+    
+    const validID = await userLookupWithID(adminid, "admin");
+    if (!validID) {
+        return res.status(401).json({message : "Permission Denied"});
+    }
+
+    const existingData = getUsersData();
+    const adminsNames = existingData.admins.map(admin => admin.name);
+    if (adminsNames.length === 0) {
+        return res.status(404).json({ message: "No admins found" });
+    }
+    return res.status(200).json({data : adminsNames});
+}
+
+exports.getMovieCount = async (req, res) => {
+    const movieCount = await getMovieCount();
+    if(!movieCount) {
+        res.status(400).json({message : "Error fetching movie count!"});
+    }
+    else {
+        return res.status(200).json({count : movieCount});
+    }
+}
+
+exports.checkAdminID = async (req, res) => {
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).json({message : "ID not provided!"});
+    }
+
+    const data = await getUsersData();
+    const presence = data.find(u => u.id === id) || null;
+    if (!presence) {
+        return res.status(401).json({message : "Access denied!"});
+    }
+
+    return res.status(200).json({message : "Admin verified successfully!"});
+}
