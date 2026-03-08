46684161f926f76339c6bcd6a0ee3ca4caf34c54
Prisha Aggarwal <prisha230906@gmail.com>
2026-03-08 13:52:51 +0530

    Removed admin routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,18 +4,12 @@ const controllers = require("../controllers/user.controller");
 const router = express.Router();
 
 router.get("/users/subscribers/:id", controllers.getUsersDataByID);
-router.get("/users/admins/:id", controllers.getAdmins);
 router.get("/movies/mylist/:id", controllers.fetchUserMovieList);
 router.get("/movies", controllers.fetchMoviesData);
-router.get("/movies/count", controllers.getMovieCount);
 router.post("/users/subscribers/register", controllers.addSubscriber);
-router.post("/users/admins/register", controllers.addAdmin);
 router.post("/users/subscribers/login", controllers.fetchSubscriber);
-// router.patch("/admins/changeinfo/:email", controllers.changeAdminInfo);
 router.patch("/users/subscribers/changeinfo", controllers.changeSubscriberInfo);
-// router.patch("/admins/changepassword/:email", controllers.changeAdminPassword);
 router.patch("/users/subscribers/changepassword", controllers.changeSubscriberPassword);
 router.delete("/users/subscribers/remove/:id", controllers.removeSubscriberAccount);
-// router.delete("/admins/remove/:email", controllers.removeAdminAccount);
 
 module.exports = router;
