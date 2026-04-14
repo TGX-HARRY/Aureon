8ebe6d10cd59dae9df0b1acfcf9ccd78f533e101
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-14 23:23:57 +0530

    User model created

---
+++ src/models/user.model.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false, 
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+    },
+
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+
+    phone: {
+      type: String,
+      default: "",
+    },
+
+    fullName: {
+      type: String,
+      default: "",
+    },
+
+    address: {
+      type: String,
+      default: "",
+    },
+
+    dob: {
+      type: Date,
+      default: null, 
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
