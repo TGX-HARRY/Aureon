68069b46a0875988f7776f85f4589d46d225f573
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-14 18:08:37 +0530

    Removed Movie routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,7 @@ const controllers = require("../controllers/user.controller");
 const router = express.Router();
 
 router.get("/users/subscribers/:id", controllers.getUsersDataByID);
-router.get("/movies/mylist/:id", controllers.fetchUserMovieList);
-router.get("/movies", controllers.fetchMoviesData);
+
 router.post("/users/subscribers/register", controllers.addSubscriber);
 router.post("/users/subscribers/login", controllers.fetchSubscriber);
 router.patch("/users/subscribers/changeinfo", controllers.changeSubscriberInfo);
