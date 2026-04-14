cfc4d53ebbc6ff9c59f5ca01f88495643df1232e
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-14 18:20:32 +0530

    Added env process, cookie parser for cookies management, movie routing and formatted code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cookieparser = require("cookie-parser");
+
+// import routes
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
+const movieRoutes = require("./routes/movie.routes");
+
+
+// start env process before server
+const env = require("dotenv");
+env.config();
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieparser(process.env.COOKIE_SECRET));
 app.use(express.static("public"));
 
 app.use("/api", userRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/movies", movieRoutes);
 
 module.exports = app;
