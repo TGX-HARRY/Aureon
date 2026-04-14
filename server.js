2f6a5b921576f6d00a923b21be5675967708eb90
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-14 19:39:26 +0530

    Added env process

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const http = require("http");
+const dotenv = require("dotenv");
+dotenv.config(); // 🔥 FIRST
+
 const app = require("./src/app");
 
-const port = 1212;
+const PORT = process.env.PORT;
 
-app.listen(port, () => {
-    console.log("API and Webpage live at http://localhost:1212");
-})
+app.listen(PORT, () => {
+  console.log(`API and Webpage live at http://localhost:${PORT}`);
+});
