098eab0f7747602c3e835de4242f414928e86ea2
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-16 08:59:57 +0530

    Added few functions to handle movie upload and fetch

--- backend/services/movie.service.js
+++ backend/services/movie.service.js
@@ -1,26 +1,59 @@
 const fs = require("fs").promises;
 const path = require("path");
+const movieModel = require("../models/movie.model");
+const userModel = require("../models/user.model");
+const sectionModel = require("../models/section.model");
 
 const moviesFilePath = path.join(__dirname, "../../data/movies.json");
 
-async function getMoviesData() {
+async function getAllMovies() {
     try {
-        const data = await fs.readFile(moviesFilePath, 'utf-8');
+        const data = await movieModel.find();
         if (!data) {
-            return null;
+            return "Data not found";
         }
         return JSON.parse(data);
+    } catch (err) {
+        return err;
     }
-    catch (err) {
-        console.error("Error reading file:", err);
-        return null;
+}
+
+async function getSectionsAndMovies() {
+    try {
+        const data = await sectionModel.find().populate('movies');
+        if (!data) {
+            return "Data not found";
+        }
+        return data;
+    } catch (err) {
+        return err;
     }
 }
 
 async function getUserMovies(id) {
-    const data = await getUsersData();
+    const data = await userModel.findById(id).select();
     const userdata = data.find(u => u.id === id);
     return userdata.movies;
 }
 
-module.exports = { getMoviesData, getUserMovies };
+async function appendMovieDb(movie) {
+    const {slug, title, img, genre, trailer} = movie;
+    if (!slug || !title || !img || !genre || !trailer) {
+        return "movie.service.js -> Details missing!"
+    }
+
+    try {
+        const upload = movieModel.insertOne({
+            slug, 
+            title, 
+            genre, 
+            img, 
+            trailer
+        });
+        return "success";
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = { getAllMovies, getSectionsAndMovies, getUserMovies, appendMovieDb };
