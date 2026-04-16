89e675780eb425f3eeb62e4c92c82331f706d026
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-16 08:44:19 +0530

    Called connectDB function to connect to DB

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cookieparser = require("cookie-parser");
-
+const dbconfig = require("./config/db")
 // import routes
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
 const movieRoutes = require("./routes/movie.routes");
 
 const app = express();
-
 app.use(express.json());
 app.use(cookieparser(process.env.COOKIE_SECRET));
 app.use(express.static("public"));
-
-app.use("/api", userRoutes);
+dbconfig.connectDB();
+app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/movies", movieRoutes);
 
