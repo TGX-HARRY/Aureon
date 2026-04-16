af639bb1232ff01a0a6934cfe26720f9579768e3
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-16 08:50:06 +0530

    Fixed routing of users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,12 +4,12 @@ const protect = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.get("/users/subscribers/:id", controllers.getUsersDataByID);
-
-router.post("/users/subscribers/register", controllers.addSubscriber);
-router.post("/users/subscribers/login", controllers.fetchSubscriber);
-router.patch("/users/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
-router.patch("/users/subscribers/changepassword", controllers.changeSubscriberPassword);
-router.delete("/users/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
+// router.get("/users/subscribers/:id", controllers.getUsersDataByID);
+router.get("/subscribers/me", protect, controllers.getUsersDataByID)
+router.post("/subscribers/register", controllers.addSubscriber);
+router.post("/subscribers/login", controllers.fetchSubscriber);
+router.patch("/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
+router.patch("/subscribers/changepassword", controllers.changeSubscriberPassword);
+router.delete("/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
 
 module.exports = router;
