e6084ed74289bd90646ab99a53decd5afdcd63f1
Hemang Mittal <mittalhemang11@gmail.com>
2026-04-19 21:24:10 +0530

    Added password encryption

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,12 +1,15 @@
 const { default: mongoose } = require("mongoose");
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 async function addUser(email, password) {
     if (!email || !password) return false;
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
     try {
         const upload = await userModel.insertOne({
-            email, 
-            password,
+            email,
+            password : hashedPassword,
         });
 
         return true;
@@ -35,7 +38,15 @@ async function getUser(email, password) {
     if (!getId) return null;
 
     const user = await userModel.findById(getId);
+    
+    
     if (user != null) {
+        const matchPasswords = await bcrypt.compare(password, user.password);
+        if (!matchPasswords) {
+            return res
+            .status(401)
+            .json({message: "Incorrect Password!"});
+        }
         const fetchedUser = {
             userId: user._id,
             username: user.name
