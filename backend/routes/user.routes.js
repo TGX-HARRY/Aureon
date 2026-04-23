8cd1cc0da7a329aba8f81b7905d7b6d21b94757e
Prisha Aggarwal <prisha230906@gmail.com>
2026-04-24 02:38:48 +0530

    Added middleware to required routes accordingly

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const controllers = require("../controllers/user.controller");
-const protect = require("../middleware/middleware");
+const middleware = require("../middleware/middleware");
 
 const router = express.Router();
 
 // router.get("/users/subscribers/:id", controllers.getUsersDataByID);
-router.get("/subscribers/me", protect, controllers.getUsersDataByID)
+router.get("/subscribers/me", middleware.protect, controllers.getUsersDataByID);
 router.post("/subscribers/register", controllers.addSubscriber);
 router.post("/subscribers/login", controllers.fetchSubscriber);
-router.patch("/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
+router.patch("/subscribers/changeinfo", middleware.protect, controllers.changeSubscriberInfo);
 router.patch("/subscribers/changepassword", controllers.changeSubscriberPassword);
-router.delete("/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
+router.delete("/subscribers/remove/:id", middleware.protect, controllers.removeSubscriberAccount);
 
 module.exports = router;
