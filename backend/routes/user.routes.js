4d0a885648f7e2894bb8f28d10d461f7cfc45ef8
mahesh4596 <maheshsingla2007@gmail.com>
2026-04-24 17:00:55 +0530

    style: finalize spacing and formatting in user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Account management routes
 router.get("/subscribers/me", protect, controllers.getUsersDataByID)
 router.patch("/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
-router.delete("/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
+router.delete("/subscribers/remove/:id", protect, controllers.removeSubscriberAccount);
 
 // Authentication routes
 router.post("/subscribers/register", controllers.addSubscriber);
