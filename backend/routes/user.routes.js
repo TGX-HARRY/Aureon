261a17dfd534d05a5b6546399050b57ec267644c
mahesh4596 <maheshsingla2007@gmail.com>
2026-04-24 17:00:27 +0530

    docs: add group comments to user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,11 +4,14 @@ const { protect } = require("../middleware/middleware");
 
 const router = express.Router();
 
+// Account management routes
 router.get("/subscribers/me", protect, controllers.getUsersDataByID)
+router.patch("/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
+router.delete("/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
+
+// Authentication routes
 router.post("/subscribers/register", controllers.addSubscriber);
 router.post("/subscribers/login", controllers.fetchSubscriber);
-router.patch("/subscribers/changeinfo", protect, controllers.changeSubscriberInfo);
 router.patch("/subscribers/changepassword", controllers.changeSubscriberPassword);
-router.delete("/subscribers/remove/:id", protect , controllers.removeSubscriberAccount);
 
 module.exports = router;
