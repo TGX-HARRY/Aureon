d35d6b0b0d64e8e6ba90656b6a898cf0e92df6d2
Hemang Mittal <mittalhemang11@gmail.com>
2026-05-17 21:34:43 +0530

    Added CORS for local environemnt

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ dotenv.config(); // 🔥 FIRST
 const express = require("express");
 const cookieparser = require("cookie-parser");
 const dbconfig = require("./config/db")
+const cors = require("cors");
 // import routes
 const userRoutes = require("./routes/user.routes");
 const adminRoutes = require("./routes/admin.routes");
 const movieRoutes = require("./routes/movie.routes");
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieparser());
 app.use(express.static("public"));
